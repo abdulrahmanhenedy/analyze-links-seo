3c507a3c7d58154ee008eff90aff5a4ad40998cc
Abdulrahman Henedy <114556029+abdulrahmanhenedy@users.noreply.github.com>
2023-02-06 05:55:58 +0200

    Update Operations.py

--- work/Operations.py
+++ work/Operations.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-df=pd.read_csv('/Users/abdulrahmanhenedy/PycharmProjects/links/Files/innerlinks.csv')
+inlinks=pd.read_csv('/Users/abdulrahmanhenedy/PycharmProjects/links/Files/innerlinks.csv')
 
-df = df.rename(columns={'url': 'Source', 'links_url': 'Target'})
+inlinks = inlinks.rename(columns={'url': 'Source', 'links_url': 'Target'})
 
 cols=['Source', 'Target']
-df=df[cols]
-df.to_csv('gephi.csv')
+inlinks=inlinks[cols]
+inlinks.to_csv('gephi.csv')
 
-inlinks=df['links_url'].value_counts()
+innerlinks=inlinks['links_url'].value_counts()
 
-inlinks.to_csv('inlinks_final.csv')
+innerlinks.to_csv('inlinks_final.csv')
